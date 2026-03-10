9c178c5c356063f02f839ad2ce2fea38db1132bb
Pooja <pooja@Saurabhs-MacBook-Pro.local>
2026-03-10 13:05:38 +0530

    updated changes for get api for user

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 const express =  require('express');
-const app =  express()
+const app =  express();
 
 // to handle request
-app.use((req,res) => {
-  res.send('Hello from server')
+
+// app.use('/hello',(req,res) => {
+//     res.send('Hello from server')
+// })
+
+// app.use('/hello/2',(req,res) => {
+//     res.send('Hello 2 from server')
+// })
+
+// app.use('/namaste',(req,res) => {
+//     res.send('namaste from server')
+// })
+
+// app.use('/', (req,res) => {
+//     res.send('server give response')
+//   })
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+    res.send({firstname: "Pooja", lastName:"devi"})
 })
-app.listen(3000,() => {
-    console.log(('app is running on 3000'))
+
+app.listen(3000,(req, res) => {
+    console.log("app is running on 3000")
 })
