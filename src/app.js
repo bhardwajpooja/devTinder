dff3357b1642edc423a5b50bf92fbc7b28b824bc
Pooja <pooja@Saurabhs-MacBook-Pro.local>
2026-03-11 12:24:30 +0530

    scheman modifications done

--- src/app.js
+++ src/app.js
@@ -51,27 +51,52 @@ app.post('/signup', async (req, res) => {
         const response = await user.save();
         console.log('response',response)
         res.send({error:false, msg: 'user added successfully'})
-    } catch(e) {
-        console.log('exception',e)
-        res.send({error: true})
+    } catch(exception) {
+        console.log('exception',exception)
+        res.send({error: true, msg: exception})
     }
  })
  
  // feed API -> get all the users from database
-
-
  app.get('/users', async (req, res) => {
 
  });
 
-// get user by email id  or // get user by id
+// get user by email id or // get user by id
  app.get('/user', async (req, res) => {
     try {
         const userEmail = req.body.emailId
-        const result = await User.find({emailId:userEmail})
+        const result = await User.find({emailId: userEmail})
         res.send(result)
-    } catch(e) {
-        res.send('error')
+    } catch(exception) {
+        res.send({error: true, msg:exception})
+    }
+ })
+
+// delete users 
+ app.delete("/user", async (req, res) => {
+    try {
+        const userId =  req.body.userId
+        // const result = await User.findByIdAndDelete({_id:userEmail})  //or below is short hand for this
+         const result = await User.findByIdAndDelete(userId)
+         res.send(result)
+    } catch(exception) {
+        res.send({error: true, msg:exception})
+    }
+ });
+
+ // update user data
+
+
+ app.patch("/user", async (req, res) => {
+    try {
+        const userId =  req.body.userId
+        const bodyParams =  req.body;
+        const result =  await User.findByIdAndUpdate(userId, bodyParams, {returnDocument: "before"})
+        // const result =  await User.findByIdAndUpdate({_id: userId}, bodyParams, {returnDocument: "before"})
+        res.send(result)
+    } catch(exception) {
+        res.send({error: true, msg:exception})
     }
  })
 
--- src/model/user.js
+++ src/model/user.js
@@ -1,24 +1,55 @@
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.Schema({
-    firstName : {
-        type : String
+const userSchema = mongoose.Schema(
+    {
+        firstName : {
+            type : String,
+            required: true,
+            minLength: 4,
+            maxLength: 50
+        },
+        lastName : {
+            type : String
+        },
+        emailId : {
+            type : String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true
+        },
+        password : {
+            type : String,
+            required: true
+        },
+        age : {
+            type : Number,
+            min: 18
+
+        },
+        gender : {
+            type : String,
+            validate (value) {
+                if(!(["male","female","others"].indexOf(value) > -1)){
+                    throw new Error("Gender is not valid");
+                }
+            } 
+        },
+        photoUrl: {
+            type: String,
+            default: "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg"
+        },
+        about: {
+            type: String,
+            default: "This is default about of user"
+        },
+        skills : {
+            type: [String]
+        } 
     },
-    lastName : {
-        type : String
-    },
-    emailId : {
-        type : String
-    },
-    password : {
-        type : String
-    },
-    age : {
-        type : Number
-    },
-    gender : {
-        type : String
+    {
+        timestamps : true
     }
-})
+)
 
 module.exports = mongoose.model("users", userSchema)
