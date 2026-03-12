819ba791546d178aa6e38d6564f5e66c5f61878f
Pooja <pooja@Saurabhs-MacBook-Pro.local>
2026-03-12 14:12:07 +0530

    logout api added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -50,6 +50,14 @@ authRouter.post('/signup', async (req, res) => {
     }
  })
 
+ authRouter.post('/logout',async (req, res) => {
+    res.cookie('token',null, {expires: new Date(Date.now())})
+    res.send('logout successfully')
+
+
+   // OR res.cookie('token',null, {expires: new Date(Date.now())}).send('logout successfully')
+ })
+
 
 
 module.exports =  authRouter;
